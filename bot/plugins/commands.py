9d28f2a7a22dc29200e6b3c4478e49ac0df47a8d
Fgroots <87475844+Fgroots@users.noreply.github.com>
2021-07-19 08:38:08 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = "@cinemazipnews",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '💢 Our Channel 💢', url="https://t.me/cinema_zip"
                                 )
                         ]
                     ]
