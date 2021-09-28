060e260d21a06de4b17d38f8f942b359e2e336f1
godgroot <85842733+godgroot@users.noreply.github.com>
2021-09-28 12:39:09 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -47,7 +47,7 @@ For More Details Click Help Button Below..
 
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> Cinemazip Auto Filter Bot</code>
     
 <b>➥ Creator</b> : <b><i><a href="https://t.me/im_godgroot">Aashaan</a></i></b>
 
@@ -55,5 +55,5 @@ For More Details Click Help Button Below..
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/Fgroots/hulkbotv2">Click Me</a></i>
+<b>➥ സൊസ്</b> : <i><a href="https://t.me/im_godgroot">Click Me</a></i>
 """
